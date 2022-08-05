e0470b1cd23706035c013a27d1e53ef2fc1eeaf9
minhkhoi1026 <minhkhoi1026@gmail.com>
2022-08-05 22:53:46 +0700

    add auto yes option to installation script

--- install_dep.sh
+++ install_dep.sh
@@ -1,3 +1,3 @@
-conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+conda install -y -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 pip install -r requirement.txt
