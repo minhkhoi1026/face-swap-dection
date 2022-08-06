bedb2c96c6348d71f5ba46ab67c7f6daf648dae2
minhkhoi1026 <minhkhoi1026@gmail.com>
2022-08-06 15:46:48 +0700

    update dependency

--- install_dep.sh
+++ install_dep.sh
@@ -1,3 +1,3 @@
-conda install -y -c conda-forge cudatoolkit=10.1 cudnn=7.6
+conda install -y -c conda-forge cudatoolkit=10.1 cudnn=7.6.5
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 pip install -r requirement.txt
