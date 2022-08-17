199f500dd4ffcbe7d0806bb590de10b3eff771d8
minhkhoi1026 <minhkhoi1026@gmail.com>
2022-08-17 23:34:47 +0700

    remove lambda layer in Attention

--- src/models/attention.py
+++ src/models/attention.py
@@ -10,7 +10,6 @@ import keras.backend as K
 from src.models.mobilenet_v3_large import MobileNetV3_Large
 from src.models.mobilenet_v3_small import MobileNetV3_Small
 
-
 class Attention(Layer):
     def __init__(self, size, **kwargs):
         self.trainable=True
@@ -18,53 +17,38 @@ class Attention(Layer):
         super(Attention, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        self.q = self.add_weight(name='q',
-                                 shape=(1, self.size),
-                                 initializer='ones',
-                                 trainable=self.trainable)
         super(Attention, self).build(input_shape)
+        self.q = self.add_weight(name='q',
+                                shape=(1, self.size),
+                                initializer='ones',
+                                trainable=self.trainable)
 
     def call(self, x):
         stream1, stream2 = x[0], x[1]
 
-
-        d1 = Lambda(lambda x: K.sum(x * self.q, axis=1, keepdims=True))(stream1) # sum over second axis
-        d2 = Lambda(lambda x: K.sum(x * self.q, axis=1, keepdims=True))(stream2)
+        d1 = K.sum(stream1 * self.q, axis=1, keepdims=True) # sum over second axis
+        d2 = K.sum(stream2 * self.q, axis=1, keepdims=True)
+        print(d1.shape, d2.shape)
         ds = Concatenate(axis=1)([d1, d2])
 
         # d1 and d2 and of size (bs, 1) individually
         # ds of size (bs, 2)
 
         tmp = Softmax(axis=0)(ds)
-        # print(tmp._keras_shape)
-        w1 = Lambda(lambda x: x[:, 0])(tmp)
-        w2 = Lambda(lambda x: x[:, 0])(tmp)
+        w1 = tmp[:, 0]
+        w2 = tmp[:, 1]
 
         w1 = Lambda(lambda x: K.expand_dims(x, -1))(w1)
         w2 = Lambda(lambda x: K.expand_dims(x, -1))(w2)
-        # print(w1._keras_shape)
-        # print(w1.shape)
-        # print(w2.shape)
 
         stream1 = Lambda(lambda x: x[0]*x[1])([stream1, w1])
         stream2 = Lambda(lambda x: x[0]*x[1])([stream2, w2])
-        # result = Lambda(lambda x: x[0]+x[1])([stream1, stream2])
         result = Add()([stream1, stream2])
-        # print(result.shape)
+        
         return result
 
     def compute_output_shape(self, input_shape):
         return input_shape[0]
-
-# stream1 = Input(batch_shape= (2, 10*10*2048,))
-# stream2 = Input(batch_shape = (2,10*10*2048,))
-# shapes = 2048*100
-# stream1 = Input((shapes,))
-# stream2 = Input((shapes,))
-# output = Attention(size=shapes)([stream1, stream2])
-# print(output._keras_shape)
-# model = Model(inputs=[stream1, stream2], outputs=output)
-# print(model.summary())
 
 def attention_model(classes, backbone = 'MobileNetV3_Small', shape=(256, 256, 3)):
     if backbone == 'Xception':
@@ -82,22 +66,19 @@ def attention_model(classes, backbone = 'MobileNetV3_Small', shape=(256, 256, 3)
     output1 = stream1(input1)
     output2 = stream2(input2)
    
-    stream1.name = "stream1"
-    stream2.name = "stream2"
+    stream1._name = "stream1"
+    stream2._name = "stream2"
     if backbone == 'Xception':
         output1 = GlobalAveragePooling2D(name='avg_pool_1')(output1)
         output2 = GlobalAveragePooling2D(name='avg_pool_2')(output2)
 
-    # stream1 = Flatten()(stream1)
-    # stream2 = Flatten()(stream2)
-    # print(stream1.shape)
     output = Attention(size=output1.shape[1])([output1, output2])
-    # print(output.shape)
+
     if classes==1:
         output = Dense(classes, activation='sigmoid', name='predictions')(output)
     else:
         output = Dense(classes, activation='softmax', name='predictions')(output)
-    # print(output.shape)
+
     return Model(inputs=[input1, input2], outputs=output)
 
 if __name__ == "__main__":
